2a118dcf3080c105d95d4e819f06795e047979d8
tomastrg <tomastrg22@gmail.com>
2019-09-14 10:45:34 +0200

    wip

--- src/utils/constants/dappContract.js
+++ src/utils/constants/dappContract.js
@@ -1,5 +1,5 @@
 import footballContest from './../footballContest.json'
 
 export const ABI = footballContest.abi
-export const address = '0x7fA3df51927F949EC4A1D232b728e80Dd9A67501'
+export const address = '0x66cbD093a3E96882EeC38827dC20F4b54bbEC77E'
 export const adminAddress = '0x59Cc5476Fb72866e24bfD529f49e3E87a5dc870d'
